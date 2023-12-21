bfab54106c351b36345fce6cd1524a990412a71a
ThanhCaKoi <118273786+ThanhCaKoi@users.noreply.github.com>
2023-12-21 21:58:39 +0700

    feat: create crud xuat nhap kho and login logout crud xuat nhap kho #7

---
+++ lichsunhaphang/sualichsunhaphang.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+    exit;
+}
+
+require('connection.php');
+$title = "Sửa lịch sử nhập hàng";
+$error = [];
+
+if (isset($_GET['id'])) {
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    $query = "SELECT l.*, n.tenNhanVien, s.tenSanPham, np.tenNhaPhanPhoi, d.tenDonVi, kh.tenKhoHang
+              FROM lichsunhaphang l
+              INNER JOIN nhanvien n ON l.nhanVienId = n.id
+              INNER JOIN sanpham s ON l.sanPhamId = s.id
+              INNER JOIN nhaphanphoi np ON l.nhaPhanPhoiId = np.id
+              INNER JOIN donvi d ON l.donViId = d.id
+              INNER JOIN khohang kh ON l.khoHangId = kh.id
+              WHERE l.id = '$id'";
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $existingNhanVienId = $row['nhanVienId'];
+        $nhanVienName = $row['tenNhanVien'];
+        $existingSanPhamId = $row['sanPhamId'];
+
+        $sanPhamName = $row['tenSanPham'];
+        $existingSoLuong = $row['soLuong'];
+
+        $existingNhaPhanPhoiId = $row['nhaPhanPhoiId'];
+        $nhaPhanPhoiName = $row['tenNhaPhanPhoi'];
+        $existingDonViId = $row['donViId'];
+
+        $donViName = $row['tenDonVi'];
+        $existingKhoHangId = $row['khoHangId'];
+
+        $khoHangName = $row['tenKhoHang'];
+        $existingThoiGian = $row['thoiGian'];
+    } else {
+        $error[] = "Không tìm thấy dữ liệu";
+    }
+}
+
+$queryNhanVien = "SELECT id, tenNhanVien FROM nhanvien";
+$resultNhanVien = mysqli_query($conn, $queryNhanVien);
+
+$querySanPham = "SELECT id, tenSanPham FROM sanpham";
+$resultSanPham = mysqli_query($conn, $querySanPham);
+
+$queryNhaPhanPhoi = "SELECT id, tenNhaPhanPhoi FROM nhaphanphoi";
+$resultNhaPhanPhoi = mysqli_query($conn, $queryNhaPhanPhoi);
+
+$queryDonVi = "SELECT id, tenDonVi FROM donvi";
+$resultDonVi = mysqli_query($conn, $queryDonVi);
+
+$queryKhoHang = "SELECT id, tenKhoHang FROM khohang";
+$resultKhoHang = mysqli_query($conn, $queryKhoHang);
+
+if (isset($_POST['submit'])) {
+    $nhanVienId = mysqli_real_escape_string($conn, $_POST['nhanVienId']);
+    $sanPhamId = mysqli_real_escape_string($conn, $_POST['sanPhamId']);
+    $soLuong = mysqli_real_escape_string($conn, $_POST['soLuong']);
+    $nhaPhanPhoiId = mysqli_real_escape_string($conn, $_POST['nhaPhanPhoiId']);
+    $donViId = mysqli_real_escape_string($conn, $_POST['donViId']);
+    $khoHangId = mysqli_real_escape_string($conn, $_POST['khoHangId']);
+    $thoiGian = mysqli_real_escape_string($conn, $_POST['thoiGian']);
+
+    $updateQuery = "UPDATE lichsunhaphang
+                    SET nhanVienId = '$nhanVienId', sanPhamId = '$sanPhamId', soLuong = '$soLuong',
+                        nhaPhanPhoiId = '$nhaPhanPhoiId', donViId = '$donViId', khoHangId = '$khoHangId',
+                        thoiGian = '$thoiGian'
+                    WHERE id = '$id'";
+
+    $updateResult = mysqli_query($conn, $updateQuery);
+
+    if ($updateResult) {
+        header("Location: lichsunhaphang.php?status=update_success");
+        exit();
+    } else {
+        $error[] = "Update failed: " . mysqli_error($conn);
+        header("Location: lichsunhaphang.php?status=update_fail");
+    }
+}
+
+?>
+
+</html>
+
+<?php if (count($error) > 0) : ?>
+    <?php foreach ($error as $errMsg) : ?>
+        <div class="alert alert-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>Error: </strong><?php echo $errMsg; ?>
+        </div>
+    <?php endforeach; ?>
+<?php endif; ?>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 </title>
+
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        <?php require('../layout/menu.php'); ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div class="content" mx-2>
+                <?php require('../layout/header.php'); ?>
+                <div class="container">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="nhanVienId">Nhân viên</label>
+                            <select class="form-control" name="nhanVienId">
+                                <?php
+                                echo "<option value=\"$existingNhanVienId\" selected>$nhanVienName</option>";
+                                while ($row = mysqli_fetch_assoc($resultNhanVien)) {
+                                    $optionId = $row['id'];
+                                    $optionName = $row['tenNhanVien'];
+
+                                    if ($optionId !== $existingNhanVienId) {
+                                        echo "<option value=\"$optionId\">$optionName</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="sanPhamId">Sản phẩm</label>
+                            <select class="form-control" name="sanPhamId">
+                                <?php
+                                echo "<option value=\"$existingSanPhamId\" selected>$sanPhamName</option>";
+                                while ($row = mysqli_fetch_assoc($resultSanPham)) {
+                                    $optionId = $row['id'];
+                                    $optionName = $row['tenSanPham'];
+
+                                    if ($optionId !== $existingSanPhamId) {
+                                        echo "<option value=\"$optionId\">$optionName</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="soLuong">Số Lượng</label>
+                            <input type="number" class="form-control" name="soLuong" value="<?php echo $existingSoLuong; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="nhaPhanPhoiId">Nhà phân phối</label>
+                            <select class="form-control" name="nhaPhanPhoiId">
+                                <?php
+                                echo "<option value=\"$existingNhaPhanPhoiId\" selected>$nhaPhanPhoiName</option>";
+                                while ($row = mysqli_fetch_assoc($resultNhaPhanPhoi)) {
+                                    $optionId = $row['id'];
+                                    $optionName = $row['tenNhaPhanPhoi'];
+
+                                    if ($optionId !== $existingNhaPhanPhoiId) {
+                                        echo "<option value=\"$optionId\">$optionName</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="donViId">Đơn vị</label>
+                            <select class="form-control" name="donViId">
+                                <?php
+                                echo "<option value=\"$existingDonViId\" selected>$donViName</option>";
+                                while ($row = mysqli_fetch_assoc($resultDonVi)) {
+                                    $optionId = $row['id'];
+                                    $optionName = $row['tenDonVi'];
+
+                                    if ($optionId !== $existingDonViId) {
+                                        echo "<option value=\"$optionId\">$optionName</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="khoHangId">Kho hàng</label>
+                            <select class="form-control" name="khoHangId">
+                                <?php
+                                echo "<option value=\"$existingKhoHangId\" selected>$khoHangName</option>";
+                                while ($row = mysqli_fetch_assoc($resultKhoHang)) {
+                                    $optionId = $row['id'];
+                                    $optionName = $row['tenKhoHang'];
+
+                                    if ($optionId !== $existingKhoHangId) {
+                                        echo "<option value=\"$optionId\">$optionName</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="thoiGian">Thời gian</label>
+                            <input type="datetime-local" class="form-control" name="thoiGian" value="<?php echo date('Y-m-d\TH:i', strtotime($existingThoiGian)); ?>" required>
+                        </div>
+                        <input type="submit" class="btn btn-primary" name="submit" value="Cập nhật">&nbsp;
+                        <input type="button" class="btn btn-primary" name="btnCancel" value="Trở về" onclick="history.back(1)">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/datatables-demo.js"></script>
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Edit Record</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Add your form fields for editing here -->
+                    <!-- For example, you can use input fields with the current values -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Delete Record</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to delete this record?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <a id="deleteRecordLink" class="btn btn-danger" href="#">Delete</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
+
+<?php
+?>
+<!-- <script type="text/javascript">
+    $(function() {
+        $('#thoiGianPicker').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm:ss',
+        });
+    });
+</script> -->
---
+++ lichsuxuathang/lichsuxuathang.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+    exit;
+}
+require('../lichsunhaphang/connection.php');
+$error = [];
+?>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 </title>
+
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <div id="wrapper">
+
+        <?php require('../layout/menu.php'); ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php require('../layout/header.php'); ?>
+                <div class="container-fluid">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <div class="container">
+                                    <?php $status = isset($_GET["status"]) ? $_GET["status"] : ""; ?>
+                                    <?php if ($status == 'add_success') : ?>
+                                        <div class="alert alert-success" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Thêm thành công</strong>
+                                        </div>
+                                    <?php elseif ($status == 'add_fail') : ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Thêm thất bại</strong>
+                                        </div>
+                                    <?php elseif ($status == 'del_success') : ?>
+                                        <div class="alert alert-success" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Xóa thành công</strong>
+                                        </div>
+                                    <?php elseif ($status == 'del_fail') : ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Xóa thất bại</strong>
+                                        </div>
+                                    <?php elseif ($status == 'update_success') : ?>
+                                        <div class="alert alert-success" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Sửa thành công</strong>
+                                        </div>
+                                    <?php elseif ($status == 'update_fail') : ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Sửa thất bại</strong>
+                                        </div>
+                                    <?php endif; ?>
+
+                                </div>
+                                <div class="row justify-content-end">
+                                    <div class="col-auto">
+                                        <a href="themlichsuxuathang.php" class="btn btn-success btn-icon-split">
+                                            <div class="icon">
+                                                <i class="fas fa-plus"></i>
+                                            </div>
+                                            <span class="text">Thêm Lịch Sử Xuất Hàng</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
+                                                <tr>
+                                                    <th>Nhân viên</th>
+                                                    <th>Sản phẩm</th>
+                                                    <th>Số lượng</th>
+                                                    <th>Nhà phân phối</th>
+                                                    <th>Đơn vị</th>
+                                                    <th>Kho hàng</th>
+                                                    <th>Thời gian</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                require('../lichsunhaphang/connection.php');
+                                                $title = "Danh sách xuất kho";
+                                                $error = [];
+                                                $sql = "SELECT l.*, nv.tenNhanVien, sp.tenSanPham, np.tenNhaPhanPhoi, dv.tenDonVi, kh.tenKhoHang
+                                                FROM lichsuxuathang l
+                                                INNER JOIN nhanvien nv ON l.nhanVienId = nv.id
+                                                INNER JOIN sanpham sp ON l.sanPhamId = sp.id
+                                                INNER JOIN nhaphanphoi np ON l.nhaPhanPhoiId = np.id
+                                                INNER JOIN donvi dv ON l.donViId = dv.id
+                                                INNER JOIN khohang kh ON l.khoHangId = kh.id";
+                                                $lichsuxuathang = mysqli_query($conn, $sql);
+                                                if (mysqli_num_rows($lichsuxuathang) > 0) {
+                                                    $stt = 1;
+                                                    foreach ($lichsuxuathang as $item) {
+                                                        echo "<tr>
+                                                    <td>{$item['tenNhanVien']}</td>
+                                                    <td>{$item['tenSanPham']}</td>
+                                                    <td>{$item['soLuong']}</td>
+                                                    <td>{$item['tenNhaPhanPhoi']}</td>
+                                                    <td>{$item['tenDonVi']}</td>
+                                                    <td>{$item['tenKhoHang']}</td>
+                                                    <td>{$item['thoiGian']}</td>
+                                                    <td style='text-align: center; display: flex'>
+                                                        <a href='chitietlichsuxuathang.php?id={$item['id']}' style='color: blue;'><i class='fas fa-exclamation-circle'></i></a>
+                                                        &nbsp;
+                                                        <a href='sualichsuxuathang.php?id={$item['id']}' style='color: green;'><i class='fas fa-edit'></i></a>
+                                                        &nbsp;
+                                                        <a onclick=\"return checkDelete('{$item['id']}');\" href='xoalichsuxuathang.php?id={$item['id']}' style='color: red;'><i class='fas fa-trash'></i></a>
+                                                    </td>
+
+                                                </tr>";
+                                                        $stt++;
+                                                    }
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Thành An Dũng Website 2023</span>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+
+    </div>
+
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/datatables-demo.js"></script>
+</body>
+
+</html>
+<script type="text/javascript">
+    function checkDelete(id) {
+        if (!confirm('Bạn chắc chắn muốn xóa lịch sử xuất hàng này?')) {
+            return false;
+        } else {
+            document.getElementById('deleteRecordLink').href = 'xoalichsuxuathang.php?id=' + id;
+            $('#deleteModal').modal('show');
+            return true;
+        }
+    }
+</script>
